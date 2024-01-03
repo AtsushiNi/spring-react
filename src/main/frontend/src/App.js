8d7d4b36f19c3b9dc90576c9c959bc08ddfec210
AtsushiNi <atsushi9731@gmail.com>
2024-01-03 09:56:22 +0900

    add mui header

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import { AppBar, CssBaseline, Link, Toolbar, Typography, Button, Container } from '@mui/material';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css';
 
 function App() {
+
+  const defaultTheme = createTheme();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles styles={{ ul: { marign: 0, padding: 0, listStyle: 'none' } }} />
+        <CssBaseline />
+        <AppBar
+          position="static"
+          color="default"
+          elevation={0}
+          sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
         >
-          Learn React
-        </a>
-      </header>
+          <Toolbar sx={{ flexWrap: 'wrap' }}>
+            <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+              Admin Page
+            </Typography>
+            <nav>
+              <Link
+                variant="button"
+                color="text.primary"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Link
+              </Link>
+              <Link
+                variant="button"
+                color="text.primary"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Link
+              </Link>
+              <Link
+                variant="button"
+                color="text.primary"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Link
+              </Link>
+            </nav>
+            <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Container maxWidth="md" component="main" sx={{ mx: 3, my: 3}}>
+          <Typography
+            component="h1"
+            variant='h2'
+            align="left"
+            color="text.primary"
+          >
+            User List
+          </Typography>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
