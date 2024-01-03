f0bb38bebf6d7953f519241e4e004e6e1532c17b
AtsushiNi <atsushi9731@gmail.com>
2024-01-03 10:06:16 +0900

    add mui table

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,4 +1,4 @@
-import { AppBar, CssBaseline, Link, Toolbar, Typography, Button, Container } from '@mui/material';
+import { AppBar, CssBaseline, Link, Toolbar, Typography, Button, Container, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css';
@@ -6,6 +6,29 @@ import './App.css';
 function App() {
 
   const defaultTheme = createTheme();
+
+  const rows = [
+    { id: 1, userName: "佐藤 太郎", division: "営業課" },
+    { id: 2, userName: "田中 花子", division: "人事課" },
+    { id: 3, userName: "鈴木 次郎", division: "開発課" },
+    { id: 4, userName: "山田 みゆき", division: "営業課" },
+    { id: 5, userName: "小林 光太", division: "総務課" },
+    { id: 6, userName: "中村 真理", division: "開発課" },
+    { id: 7, userName: "林 良一", division: "人事課" },
+    { id: 8, userName: "渡辺 美香", division: "総務課" },
+    { id: 9, userName: "斉藤 勇太", division: "開発課" },
+    { id: 10, userName: "岡田 さやか", division: "営業課" },
+    { id: 11, userName: "佐々木 裕子", division: "人事課" },
+    { id: 12, userName: "松本 啓太", division: "総務課" },
+    { id: 13, userName: "井上 貴子", division: "開発課" },
+    { id: 14, userName: "三浦 雄一", division: "営業課" },
+    { id: 15, userName: "高橋 みさ", division: "人事課" },
+    { id: 16, userName: "西村 大介", division: "開発課" },
+    { id: 17, userName: "松田 美希", division: "総務課" },
+    { id: 18, userName: "枝村 和彦", division: "開発課" },
+    { id: 19, userName: "大西 由美", division: "営業課" },
+    { id: 20, userName: "石井 勇介", division: "人事課" }
+  ]
 
   return (
     <div className="App">
@@ -54,7 +77,7 @@ function App() {
           </Toolbar>
         </AppBar>
 
-        <Container maxWidth="md" component="main" sx={{ mx: 3, my: 3}}>
+        <Container maxWidth="md" component="main" sx={{ mx: 3, my: 3 }}>
           <Typography
             component="h1"
             variant='h2'
@@ -63,6 +86,32 @@ function App() {
           >
             User List
           </Typography>
+
+          <TableContainer component={Paper}>
+            <Table sx={{ minWith: 650 }} aria-label="user table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell>User Name</TableCell>
+                  <TableCell>Division</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.id}
+                    </TableCell>
+                    <TableCell>{row.userName}</TableCell>
+                    <TableCell>{row.division}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Container>
       </ThemeProvider>
     </div>
